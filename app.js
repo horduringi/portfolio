a817aba543c1b2f5f3170e5506740b0eac86a215
Hörður <horduringi@gmail.com>
2014-10-16 22:14:57 +0000

    added top border to static page

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-var showHeader = function(i) {
-
-	$("header h2:nth-child(" + i + ")").addClass("in");
-	$("header h2:nth-child(" + i + ")").removeClass("hidden");
-	if(i < 6)
-	{
-		window.setTimeout(function () {
-			$("header h2:nth-child(" + i + ")").addClass("out");
-		    window.setTimeout(function() {
-		    	$("header h2:nth-child(" + i + ")").addClass("hidden");
-		    	showHeader(i + 1);
-		    }, 1200);
-		    
-		}, 1500);
-	}
-	else
-	{
-		$('h1').addClass('top-border');
-	}
-}
-
-
-var showSubHeader = function(i) {
-	window.setTimeout(function() {
-		$('header').append('<h2 class="text-primary">' + subHeaders[i] + '</h2>');
-		$('header h2').hide().fadeIn(1000);
-		window.setTimeout(function() {
-			$('header h2').fadeOut(1000);
-			showSubHeader(i + 1);
-			window.setTimeout(function() {
-				$('header h2').remove();		
-			}, 1000);
-		}, 1000);
-	}, 1000);
-}
-var subHeaders = ["tomorrow's web is", "usable,", "responsive,", "and simple,", "let's get started"];
 $(document).ready(function() {
-	
 	window.setTimeout(function() {
 		$('header').append("<h2 class='text-primary'>tomorrow's web is</h2>");
 		$('header h2').hide().fadeIn(1000);
@@ -69,14 +32,10 @@ $(document).ready(function() {
 	}, 7000);
 	window.setTimeout(function() {
 		$('header h2').remove();
-		$('h1').addClass('top-border');
 		$('header').append("<h2 class='text-primary'>let's get started</h2>");
 		$('header h2').hide().fadeIn(1000);
 	}, 9000);
-	//showSubHeader(0);
-	//window.setTimeout(function() {
-	//	showSubHeader(1);
-	//}, 4000);
+
 
 	window.setTimeout(function () {
 	    $("#me").addClass("in");
@@ -87,5 +46,4 @@ $(document).ready(function() {
 	window.setTimeout(function () {
 	    $("#us").addClass("in");
 	}, 18000);
-	//$("header h2:nth-child(2)").removeClass("in")
 })
