418cab7e85cdb204a7b41d580d8b3e1895a98b8b
Hörður <horduringi@gmail.com>
2014-10-18 08:56:14 +0000

    fixed mobile version and added chevron-up

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
 	}, 1000);
 	window.setTimeout(function() {
 		$('header h2').remove();
-		$('header').append("<h2 class='text-primary'>usable,</h2>");
+		$('header').append("<h2 class='text-primary'>usable, responsive, and simple,</h2>");
 		$('header h2').hide().fadeIn(1000);
 		window.setTimeout(function(){
 			$('header h2').fadeOut(1000);
 		}, 1000);
 	}, 3000);
-	window.setTimeout(function() {
+	/*window.setTimeout(function() {
 		$('header h2').remove();
 		$('header').append("<h2 class='text-primary'>responsive,</h2>");
 		$('header h2').hide().fadeIn(1000);
@@ -29,31 +29,37 @@ $(document).ready(function() {
 		window.setTimeout(function(){
 			$('header h2').fadeOut(1000);
 		}, 1000);
-	}, 7000);
+	}, 7000);*/
 	window.setTimeout(function() {
 		$('header h2').remove();
-		$('header').append("<h2 class='text-primary'>let's get started</h2>");
+		$('header').append("<h2 class='text-primary'>let's make it today</h2>");
 		$('header h2').hide().fadeIn(1000);
-	}, 9000);
+	}, 5000);
 
 
 	window.setTimeout(function () {
 	    $("#me").addClass("in");
-	}, 12000);
+	}, 7000);
 	window.setTimeout(function () {
 	    $("#myself").addClass("in");
-	}, 12000);
+	}, 7000);
 	window.setTimeout(function () {
 	    $("#us").addClass("in");
-	}, 12000);
+	}, 7000);
 
 	window.setTimeout(function () {
 	    $("#scroll-chevron-down").addClass("in");
-	}, 12000);
+	}, 7000);
 
 	$('#scroll-chevron-down').on('click', function(){
 		$("html, body").animate(
 	      {scrollTop: $("#screen-below").offset().top}
+	    , 500)
+
+	})
+	$('#scroll-chevron-up').on('click', function(){
+		$("html, body").animate(
+	      {scrollTop: $("body").offset().top}
 	    , 500)
 
 	})
